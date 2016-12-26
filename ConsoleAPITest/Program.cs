d1daa15330dc20fbd4e651eaceedccdb36cf6f3d
chaofenghai007 <chaofenghai007@sina.com>
2016-12-26 15:56:53 +0800

    增加api测试功能

---
+++ ConsoleAPITest/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAPITest
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var t1 = Task.Run(() => FetchData(1));
+            var t2 = Task.Run(() => FetchData(2));
+            var t3 = Task.Run(() => FetchData(3));
+
+            var index = Task.WaitAny(t1, t2, t3);
+            Console.WriteLine("Task {0} finished first", index + 1);
+
+            Task.WaitAll(t1, t2, t3);
+            Console.WriteLine("All tasks have finished");
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey(true);
+        }
+
+        static void FetchData(int clientNumber)
+        {
+            var client = new WebClient();
+            string data = client.DownloadString("http://localhost:18579/api/MyName");
+            Console.WriteLine("Client {0} got data: {1}", clientNumber, data);
+        }
+
+        /*
+        static void Main(string[] args)
+        {
+
+            string url = "http://localhost:18579/api/values";
+           // string postDataStr = "status=0&pi=0&ps=10";
+            //初始化request参数
+            string postData = string.Format(@"{{ ID:'{0}',NAME:'{1}',CREATETIME:'{2}' }}", 1, "Jim", "1988-09-11");// "{ ID: \"1\", NAME: \"Jim\", CREATETIME: \"1988-09-11\" }";
+          //  string postData = "{ ID: '1', NAME: 'Jim', CREATETIME: '1988-09-11' }";
+            //  var tp1= GetRemotedata(url + "?" + postDataStr);
+            HttpPostPut(url, postData, "POST");
+            url = "http://localhost:18579/api/values/2";
+            HttpPostPut(url, postData, "PUT");
+
+            Console.Read();
+        }
+        */
+        static void HttpPostPut(string url, string postData, string HttpType)
+        {          
+
+            //定义request并设置request的路径
+            WebRequest request = WebRequest.Create(url);
+            request.Method = HttpType;
+
+            //初始化request参数
+            //string postData = "{ ID: \"1\", NAME: \"Jim\", CREATETIME: \"1988-09-11\" }";
+
+            //设置参数的编码格式，解决中文乱码
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            //设置request的MIME类型及内容长度
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+
+            //打开request字符流
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            //定义response为前面的request响应
+            WebResponse response = request.GetResponse();
+
+            //获取相应的状态代码
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            //定义response字符流
+            dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();//读取所有
+            Console.WriteLine(responseFromServer);
+        }
+    }
+}
---
+++ ConsoleThread/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleThread
+{
+    class Program
+    {
+        private static ManualResetEventSlim mrs = new ManualResetEventSlim(false, 2047);
+        private static int StartId = int.Parse(ConfigurationManager.AppSettings["startId"].ToString());
+        private static int AddCount = int.Parse(ConfigurationManager.AppSettings["addCount"].ToString());
+        private static string DeviceId = ConfigurationManager.AppSettings["DeviceId"].ToString();
+        static void Main(string[] args)
+        {
+            int num = 0;
+            while (!Program.IsStart())
+            {
+                Console.WriteLine("{0}:还没有开始运行，等待0.5秒再测试！--{1}", num, DateTime.Now.ToString("HH:mm:ss:fff"));
+                Thread.Sleep(500);
+                num++;
+            }
+            Console.WriteLine("开始测试！--{0}", DateTime.Now.ToString("HH:mm:ss:fff"));
+            Program.StartThread();
+            Console.Read();
+        }
+        private static void StartThread()
+        {
+            int startId = Program.StartId;
+            for (int i = Program.StartId; i < startId + Program.AddCount; i++)
+            {
+                Task.Factory.StartNew(delegate(object obj)
+                {
+                    string result = Program.Run(obj);
+                    Console.WriteLine("执行任务成功，返回{0}", result);
+                    //string strSql=""
+                }, i);
+            }
+            Console.WriteLine("当前时间:{0}我是主线程{1}，你们这些任务都等 2s 执行吧：\n", DateTime.Now, Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Program.mrs.Set();
+        }
+
+        private static string Run(object obj)
+        {
+            Program.mrs.Wait();
+            Console.WriteLine("当前时间:{0}任务 {1}已经进入。", DateTime.Now.ToString("HH:mm:ss:fff"), obj);
+            return Program.HttpPost(obj.ToString(), DateTime.Now.ToString("HH:mm:ss:fff"));
+        }
+
+        private static void FetchData(int clientNumber)
+        {
+            var client = new WebClient();
+            string data = client.DownloadString("http://localhost:18579/api/MyName");
+            Console.WriteLine("Client {0} got data: {1}", clientNumber, data);
+        }
+
+
+        private static string HttpPost(string first, string second)
+        {
+            string text = DateTime.Now.ToString("HH:mm:ss:fff");
+            string text2 = string.Format("first={0}&second={1}&three={2}&DeviceId={3}", new object[]
+		{
+			first,
+			second,
+			text,
+			Program.DeviceId
+		});
+            Console.WriteLine("执行任务完成:{0}-- {1}已经进入。", text2, text);
+            string requestUriString = "http://192.168.1.33:8090/api/MyName/Post";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestUriString);
+            httpWebRequest.Method = "POST";
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            httpWebRequest.ContentLength = (long)Encoding.UTF8.GetByteCount(text2.ToString());
+            Stream requestStream = httpWebRequest.GetRequestStream();
+            StreamWriter streamWriter = new StreamWriter(requestStream, Encoding.GetEncoding("gb2312"));
+            streamWriter.Write(text2);
+            streamWriter.Close();
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            Stream responseStream = httpWebResponse.GetResponseStream();
+            StreamReader streamReader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+            string text3 = streamReader.ReadToEnd();
+            streamReader.Close();
+            responseStream.Close();
+            return text3;
+        }
+
+        private static bool IsStart()
+        {
+            string requestUriString = "http://192.168.1.33:8090/api/Values/GetStatus";
+            string text = "";
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestUriString);
+                httpWebRequest.Method = "Get";
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.Append(streamReader.ReadToEnd());
+                    httpWebResponse.Close();
+                    text = stringBuilder.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return !string.IsNullOrEmpty(text) && !(text.Replace("\"", "") == "0");
+        }
+
+        private static string GetRemotedata(string uri)
+        {
+            uri = uri + "&v=" + DateTime.Now.Ticks;
+            string result;
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+                httpWebRequest.Method = "Get";
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.Append(streamReader.ReadToEnd());
+                    httpWebResponse.Close();
+                    result = stringBuilder.ToString();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            result = string.Empty;
+            return result;
+        }
+    }
+}
